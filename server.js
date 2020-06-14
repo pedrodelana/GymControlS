bb29b5cbefa2a7a36d90ae18a591a0407e276c59
Pedro Lana <pedrolana@hotmail.com>
2020-06-14 07:53:12 -0300

    Subindo o basico

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const routes = require('./routes');
+
+const server = express();
+
+server.use(routes);
+server.use(express.static('public'));
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+  });
+
+server.listen(5000, function(){
+    console.log("Server is runing");
+})
